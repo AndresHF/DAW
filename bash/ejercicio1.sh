df642ca6093adbc5023eb364eea7ff48bfbfdf30
JaGUaRsPork <andresherrerof@gmail.com>
2018-02-11 18:15:06 +0100

    Learning bash commands with Surtich

--- bash/ejercicio1.sh
+++ bash/ejercicio1.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-cd ~
+#-------------------------------- EJERCICIO 1 ---------------------------------------------------------------------------
+# 1. Cree un directorio que se llame ejercicio1
+cd ~/REPASO/ej1
+mkdir ejercicio1
+# 2. Situese en ejercicio1
+cd ejercicio1
+# 3. Compruebe que está en ejercicio1
 pwd
-inicialCreacion=$(date +%s)
-echo kk2/{1..120000} | xargs mkdir -p
-finalCreacion=$(date +%s)
-sumaCreacion=${finalCreacion}-${inicialCreacion}
-echo ${sumaCreacion} | bc
-inicialBorrado=$(date +%s)
-rm -r kk2
-finalBorrado=$(date +%s)
-sumaBorrado=${finalBorrado}-${inicialBorrado}
-echo ${sumaBorrado} | bc
-echo -n "Total en creación: ";echo ${sumaCreacion} | bc;
-echo -n "Total en borrado: ";echo ${sumaBorrado} | bc;
-echo -n "TOTAL: ";echo ${sumaCreacion}+${sumaBorrado} | bc 
+# 4. Cree 52 directorios 1 por cada semana del año y que cada semana tenga un directorio para cada dia de la semana
+mkdir -p {01..52}/{1..7}
+# 5. Situese en la semana 30 día 4, desde allí cree un archivo en cada directorio día que se llame kk
+cd 30/4; touch ../../{01..52}/{1..7}/kk
+# 6. Cree un directorio que se llame 53 (a nivel de las semanas), cree el archivo kk2 con la fecha y hora actual con comando date, 
+# formato(20 14:09:09) y lo lleve a kk2 directorio 53
+mkdir ../../53; date +%d" "%k:%M:%S > kk2; mv kk2 ../../53
+# 7. Busque todos los archivos con extensión java y guarde las rutas de acceso en 02/3/java
+find / -name *.java 2> /dev/null > ../../02/3/java
+# 8. Cree en el directorio actual un enlace simbólico blando al directorio 01/7 con el nombre 7
+ln -s ../../01/7 7
+# 9. Copie todos las semanas menores a 10 en el directorio kk2, kk2 creado en el que estoy
+mkdir kk2; cp -r ../../{01..09} kk2
+# 10. Con trayectoria relativa sitúese en el directorio de trabajo y borre ejercicio1
+cd ~/REPASO/ej1; #rm -r ejercicio1
+
+
+
+
+
--- bash/ejercicio3.sh
+++ bash/ejercicio3.sh
@@ -1,52 +1,50 @@
 #!/bin/bash
-# 1. Cree un directorio que se llame ej3
-mkdir ej3
-# 2. Vaya a ej3
-cd ej3
+#-------------------------EJERCICIO 3-------------------------------------------
+
+# 1. Cree un directorio que se llame ejercicio3
+cd ~/REPASO/ej3
+mkdir ejercicio3
+
+# 2. Vaya a ejercicio3
+cd ejercicio3
+
 # 3. Compruebe donde está
 pwd
-# 4. Cuente cuatas letras tiene cada usuario
-awk -F: '{print $1}' /etc/passwd | xargs -I{} bash -c "echo {} | wc -c"
+
+# 4. Cuente cuantas letras tiene cada usuario
+cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c "echo {} | tr -d '\n' | wc -c"
 
 # 5. Muestre el usuario que más letras tiene
-awk -F: '{print $1}' /etc/passwd | xargs -I{} bash -c "echo {} | grep -o '.' | wc -l | tr '\n' ' '; echo {}" | sort -k1 -n | tail -1
+cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c "echo -n {}' ' ; echo {} | tr -d '\n' | wc -c" | sort -k2 -n | tail -1 | cut -f1 -d" "
 
-# 6. Cuáles son los usuarios que más letras tienen
-awk -F: '{print $1}' /etc/passwd | xargs -I{} bash -c 'echo {} | grep -o "." | wc -l' | sort -n | tail -1 | xargs -I{} bash -c 'cat /etc/passwd | cut -f1 -d: | egrep ".{{}}"'
-# 6. OTRA FORMA
-cat /etc/passwd | cut -f1 -d: | egrep ".{$(awk -F: '{print length($1)|"sort -n | tail -1"}' /etc/passwd)}"
+# 6. ¿Cuáles son los usuarios que más letras tienen?
+cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c "echo {} | tr -d '\n' | wc -c" | sort -n | tail -1 | xargs -I{} egrep ".{{}}" <(cat /etc/passwd | cut -f1 -d:)
+# 6.1 AWK
+awk -F: '{print length($1)|"sort -n|tail -1"}' /etc/passwd | xargs -I{} egrep ".{{}}" <(cat /etc/passwd | cut -f1 -d:)
 
-# 7. Mostrar cuántas letras diferentes  tiene cada usuario
-awk -F: '{print $1}' /etc/passwd | xargs -I{} bash -c "echo USUARIO: {}; echo {} | grep -o '.' | sort | uniq -c"
+# 7. Mostrar cuántas letras diferentes tiene cada usuario
+cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c 'echo {} | tr "\n" ":";echo {} | egrep -o "." | sort | uniq | wc -l'
 
 # 8. Mostrar cuál es el número máximo de letras diferentes que tienen los usuarios
-awk -F: '{print $1}' /etc/passwd | xargs -I{} bash -c "echo USUARIO: {}' ';echo {} | grep -o '.' | sort | uniq | wc -l" | sort -n | tail -1
+cat /etc/passwd | cut -f1 -d: | xargs -I{} bash -c 'echo {} | tr "\n" " ";echo {} | egrep -o "." | sort | uniq | wc -l' | sort -k2 -n | tail -1 | cut -f2 -d" "
 
-# 9. Construir tu propio groups para todos los user
-awk -F: '{print $1" "$4}' /etc/passwd | xargs -I{} bash -c 'echo "--"{} | cut -f1 -d" " | tr "\n" ":"; echo {} | egrep -e "([,:]$(echo {} | cut -f1 -d" ")(,|$))|(:$(echo {} | cut -f2 -d" "))" /etc/group | cut -f1 -d: | tr "\n" " ";echo'
+# 9. Contruir tu propio groups para todos los usuarios
+cat /etc/passwd | cut -f1,4 -d: | xargs -I{} bash -c 'echo {} | cut -f1 -d: | tr "\n" ":";egrep -e "(:$(echo {} | cut -f2 -d:):|(,|)$(echo {} | cut -f1 -d:)$)" /etc/group | cut -f1 -d: | tr "\n" " "; echo'
 
-# 10. Mostrar aquellos grupos que no tienen usuarios(sólo /etc/group)
-grep -e ":$" /etc/group
+# 10. Mostrar aquellos grupos que no tienen usuario (sólo /etc/group)
+egrep -e ":$" /etc/group
 
-# 11. Mostrar los grupos que más usuarios tienen
-egrep -ve ":$" /etc/group | xargs -I{} bash -c 'echo {} | cut -f1 -d: | tr "\n" " ";echo {} | cut -f4 -d: | tr "," " " | wc -w' | xargs -I{} bash -c 'echo {} | egrep "$(echo {} | cut -f2 -d" " | sort -n | tail -1)$"'
-# 12. Saque el proceso con PID más alto (ps)
-ps -e | sort -k4 -n | tail -1 | tr -s ' ' ' ' | cut -f4 -d' '
+# 11. Mostrar los grupos que más usuarios tienen (sólo /etc/group)
+egrep -e ":x:.{0,}:.{1,}" /etc/group | cut -f4 -d: | tr "," " " | xargs -I{} bash -c "echo {} | wc -w" | sort -n | tail -1 | xargs -I{} echo {}-1 | bc | xargs -I{} egrep -e ":.{1,}(,.{1,}){{}}" /etc/group | cut -f1 -d:
 
-# 13. Cuente cuantos procesos bash se están ejecutando
-ps -e | grep bash | wc -l #MAL
-pgrep -c -x "bash"
-pgrep -c "^bash$"
+# 12. ¿Cuál es el proceso con el PID más alto?
+ps -e | sort -k1 -n | tail -1 | tr -s " " " " | cut -f4 -d" "
 
-# 14. Para todos los pid y sus ppid muestre -> ppid: pid pid pid pid pid ...
-ps -e -o ppid | sort | uniq | xargs -I{} bash -c 'echo {}: | tr "\n" " "; ps -e -o pid,ppid | tr -s " " " " | grep " {}$" | cut -f2 -d" " | tr "\n" " ";echo'
-#OTRA
-ps -eo ppid --no-headers | tr -d " " | sort -n | uniq | xargs -I{} bash -c 'echo -n {}: ; ps -eo pid,ppid | egrep " {}$" | tr -s " " | egrep -o "(^| )[0-9]+ " | tr -d " " | tr "\n" " ";echo'
+# 13. Cuente los procesos bash que se están ejecutando
+ps -eo comm | grep "bash" | wc -l
 
-
-
-
-
+# 14. Para todos los pid y sus ppid muestre -> ppid: pid pid pid pid.....
+ps -eo ppid | sort -n | uniq | tr -d " " | grep -v "[A-Z]" | xargs -I{} bash -c 'echo --{} | tr "\n" ":"; ps -eo pid,ppid | tr -s " " " " |egrep "{}$" | cut -f1,2 -d" " | tr "\n" " ";echo'
 
 
 
