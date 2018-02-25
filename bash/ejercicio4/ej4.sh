48318a0fe4651ec22c15757d2f69505a0702ec14
Andrés Herrero <andresherrerof@gmail.com>
2018-02-25 11:10:24 +0100

    Shell basics 4 (for and while loops)

--- bash/ejercicio4/ej4.sh
+++ bash/ejercicio4/ej4.sh
@@ -6,7 +6,7 @@ gid=$(egrep -e "^$1:" /etc/group | cut -f3 -d:)
 collateral=$(egrep -e "^$1:" /etc/group | cut -f4 -d: | tr ',' ' ')
 
 echo -n $group: $collateral" "
-
+IFS=$'\n'
 for line in $(cat /etc/passwd); do
 	echo $line | egrep ".+:.+:.+:$gid:.+" | cut -f1 -d: | tr '\n' ' '
 done
